1ac69d6ec902d0c1f4216464aad73d89c2be893e
Dieguidev <diegogaraycullas@gmail.com>
2024-12-09 07:22:18 -0500

    Añadir manejo global de excepciones y clase ApiError para respuestas de error

---
+++ src/main/java/com/dieguidev/spring_security/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.dieguidev.spring_security.exception;
+
+import com.dieguidev.spring_security.dto.ApiError;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handlerGenericException(HttpServletRequest request, Exception e) {
+        ApiError apiError = new ApiError();
+        apiError.setBackendMessage(e.getLocalizedMessage());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMethod(request.getMethod());
+        apiError.setTimestamp(LocalDateTime.now());
+        apiError.setMessage("Error interno en el servidor");
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handlerMethodArgumentNotValidException(HttpServletRequest request,
+                                                                    MethodArgumentNotValidException e) {
+        ApiError apiError = new ApiError();
+        apiError.setBackendMessage(e.getLocalizedMessage());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMethod(request.getMethod());
+        apiError.setTimestamp(LocalDateTime.now());
+        apiError.setMessage("Error en la peticion enviada");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
+}
