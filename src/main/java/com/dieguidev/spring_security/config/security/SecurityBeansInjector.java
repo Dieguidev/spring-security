2360b099e1a4f1522ecbfbaf969df52dbb3abc12
Dieguidev <diegogaraycullas@gmail.com>
2024-12-09 07:42:13 -0500

    add: Añadir beans de AuthenticationManager y AuthenticationProvider en SecurityBeansInjector

--- src/main/java/com/dieguidev/spring_security/config/security/SecurityBeansInjector.java
+++ src/main/java/com/dieguidev/spring_security/config/security/SecurityBeansInjector.java
@@ -4,16 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
 public class SecurityBeansInjector {
 
-    @Autowired
-    private AuthenticationConfiguration authenticationConfiguration;
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationStrategy = new DaoAuthenticationProvider();
+        authenticationStrategy.setPasswordEncoder(null);
+        authenticationStrategy.setUserDetailsService(null);
+
+        return authenticationStrategy;
     }
 }
