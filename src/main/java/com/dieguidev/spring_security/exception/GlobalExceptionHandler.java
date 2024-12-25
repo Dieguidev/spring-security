4ba106ccff43434eb569c2d9a2c5da41bbf1420b
Dieguidev <diegogaraycullas@gmail.com>
2024-12-25 18:16:16 -0500

    Manejo de excepciones para autorización basada en métodos seguros

--- src/main/java/com/dieguidev/spring_security/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dieguidev/spring_security/exception/GlobalExceptionHandler.java
@@ -4,14 +4,17 @@ import com.dieguidev.spring_security.dto.ApiError;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handlerGenericException(HttpServletRequest request, Exception e) {
         ApiError apiError = new ApiError();
@@ -23,6 +26,21 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
     }
+
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handlerAccessDeniedException(HttpServletRequest request, AccessDeniedException e) {
+        ApiError apiError = new ApiError();
+        apiError.setBackendMessage(e.getLocalizedMessage());
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMethod(request.getMethod());
+        apiError.setTimestamp(LocalDateTime.now());
+        apiError.setMessage("Acceso denegado. No tienes los permisos necesarios para acceder a esta función. " +
+                "Por favor, contacta al administrador si crees que esto es un error.");
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(apiError);
+    }
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handlerMethodArgumentNotValidException(HttpServletRequest request,
